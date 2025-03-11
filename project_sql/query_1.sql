ea59c3606d40353d52b2602b4e0ababd81726e10
DanielTomaro <danieltomaro@icloud.com>
2025-03-12 09:14:00 +1100

    Query 2 complete + add results

--- project_sql/query_1.sql
+++ project_sql/query_1.sql
@@ -26,4 +26,99 @@ ORDER BY
 
 LIMIT 10;
 
-/* By using rank the query is now able to be manipulated better */
+/* By using rank the query is now able to be manipulated better
+RESULTS
+
+[
+  {
+    "job_title": "Data Analyst",
+    "company_name": "Mantys",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "650000.0",
+    "job_posted_date": "2023-02-20 15:13:33",
+    "rank": "1"
+  },
+  {
+    "job_title": "Director of Analytics",
+    "company_name": "Meta",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "336500.0",
+    "job_posted_date": "2023-08-23 12:04:42",
+    "rank": "2"
+  },
+  {
+    "job_title": "Associate Director- Data Insights",
+    "company_name": "AT&T",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "rank": "3"
+  },
+  {
+    "job_title": "Data Analyst, Marketing",
+    "company_name": "Pinterest Job Advertisements",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "232423.0",
+    "job_posted_date": "2023-12-05 20:00:40",
+    "rank": "4"
+  },
+  {
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "company_name": "Uclahealthcareers",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "217000.0",
+    "job_posted_date": "2023-01-17 00:17:23",
+    "rank": "5"
+  },
+  {
+    "job_title": "Principal Data Analyst (Remote)",
+    "company_name": "SmartAsset",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "rank": "6"
+  },
+  {
+    "job_title": "Director, Data Analyst - HYBRID",
+    "company_name": "Inclusively",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "189309.0",
+    "job_posted_date": "2023-12-07 15:00:13",
+    "rank": "7"
+  },
+  {
+    "job_title": "Principal Data Analyst, AV Performance Analysis",
+    "company_name": "Motional",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "189000.0",
+    "job_posted_date": "2023-01-05 00:00:25",
+    "rank": "8"
+  },
+  {
+    "job_title": "Principal Data Analyst",
+    "company_name": "SmartAsset",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "186000.0",
+    "job_posted_date": "2023-07-11 16:00:05",
+    "rank": "9"
+  },
+  {
+    "job_title": "ERM Data Analyst",
+    "company_name": "Get It Recruit - Information Technology",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "184000.0",
+    "job_posted_date": "2023-06-09 08:01:04",
+    "rank": "10"
+  }
+]
+ */
