f56c747764b45aca7377bb22cae2c73b716011c1
DanielTomaro <danieltomaro@icloud.com>
2025-03-12 08:53:20 +1100

    Query 1 complete

--- project_sql/query_1.sql
+++ project_sql/query_1.sql
@@ -1,5 +1,26 @@
--- Query 1: What are the top paying jobs in the dataset?
+/* Query 1: What are the top paying jobs in the dataset for 
+remote data analyst positions?
+*/
 
+SELECT
+    job_title,
+    cd.name AS company_name
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM 
+    job_postings_fact jpf
 
+LEFT JOIN
+    company_dim cd ON jpf.company_id = cd.company_id
 
+WHERE 
+    job_title_short = 'Data Analyst' AND 
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
 
+ORDER BY 
+    salary_year_avg DESC
+
+LIMIT 10;
